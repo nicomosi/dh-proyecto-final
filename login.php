e33fc8d2d30211ed5e5073902d12be4b3acb1b46
nicomosi <nico_mosi@hotmail.com>
2018-08-21 19:23:29 -0300

    home nuevo 2

--- login.php
+++ login.php
@@ -8,10 +8,10 @@
   <meta name="author" content="Nicolas Mosi">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-  <link rel="stylesheet" href="../css/estilos-nico.css">
-  <link rel="stylesheet" href="../css/estilos-francis.css">
-  <link rel="stylesheet" href="../css/estilos-juan.css">
-  <title>Home</title>
+  <link rel="stylesheet" href="css/estilos-nico.css">
+  <link rel="stylesheet" href="css/estilos-francis.css">
+  <link rel="stylesheet" href="css/estilos-juan.css">
+  <title>Login</title>
 </head>
 
 
